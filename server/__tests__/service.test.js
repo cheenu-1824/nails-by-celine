e7407b1b0a8e6dc7e7e71a303f59029015d71e7a
cheenu-1824 <cheenusadu@gmail.com>
2026-03-13 17:46:27 +1030

    Fixed issue with jest

--- server/__tests__/service.test.js
+++ server/__tests__/service.test.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 const serviceRoutes = require('../routes/serviceRoutes');
+const Service = require('../models/service');
 
 let mongoServer;
 const app = express();
@@ -13,7 +14,10 @@ app.use('/api/services', serviceRoutes);
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const uri = mongoServer.getUri();
+
   await mongoose.connect(uri);
+
+  await Service.create({ name: 'Test Service', price: 50 });
 });
 
 afterAll(async () => {
@@ -27,5 +31,19 @@ describe('GET /api/services', () => {
     const res = await request(app).get('/api/services');
     expect(res.statusCode).toEqual(200);
     expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0]).toHaveProperty('name', 'Test Service');
+  }, 10000);
+});
+
+describe('POST /api/services', () => {
+  it('should create a new service', async () => {
+    const res = await request(app)
+      .post('/api/services')
+      .send({ name: 'New Service', price: 75 });
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty('name', 'New Service');
+    expect(res.body).toHaveProperty('price', 75);
   }, 10000);
 });
